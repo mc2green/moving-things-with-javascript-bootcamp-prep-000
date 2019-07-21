3a823bfcf4132f083d5473e23c21575e774f9077
Matthew Chen <mc.2green@gmail.com>
2019-07-21 20:28:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ function moveDodgerRight() {
   var right = parseInt(rightNumbers, 10)
  
   if (right > 0) {
-    dodger.style.left = `${left - 1}px`
+    dodger.style.right = `${right - 1}px`
   }
 }
