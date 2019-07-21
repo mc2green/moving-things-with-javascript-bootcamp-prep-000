b4cdc050c376718cb4b7f439b1af0c2c3983ade8
Matthew Chen <mc.2green@gmail.com>
2019-07-21 20:22:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const app = "I don't do much."
 var dodger = document.getElementById('dodger')
 dodger.style.backgroundColor = '#FF69B4'
+
+
+dodger.style.left // "180px"
+dodger.style.bottom // "0px"
