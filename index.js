6e23075d170b98bd6434b1f50daf625d9e9efa45
Matthew Chen <mc.2green@gmail.com>
2019-07-21 20:27:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 const app = "I don't do much."
 var dodger = document.getElementById('dodger')
+function moveDodgerRight() {
+  var rightNumbers = dodger.style.left.replace('px', '')
+  var right = parseInt(rightNumbers, 10)
+ 
+  if (right > 0) {
+    dodger.style.left = `${left - 1}px`
+  }
+}
